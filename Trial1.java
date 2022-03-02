34d7b0fd378aaecfe2479ce8a4533f90b3f0f0dd
Aslam Ghumra <36637252+janitorx@users.noreply.github.com>
2022-03-02 15:25:21 +0000

    Upload trail.java
    Java script to read data

---
+++ Trial1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Trial1 {
+
+
+	public static void main ( String args [] ) {
+		
+		String mdata = "M";	
+		String fdata = "F";	
+		String odata = "O";	
+		int mvalue = 0;
+		int fvalue = 0;
+		int ovalue = 0;
+		
+		if ( mdata = "M" ) {
+			mvalue = mvalue + 1;		
+		} else if ( mdata = "F" ) {
+			fvalue = fvalue + 2;	
+		} else {
+			ovalue = ovalue + 3;
+		}
+
+	System.out.println(mvalue);
+	System.out.println(fvalue);
+	System.out.println(ovalue);
+	}
+}
